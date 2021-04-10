06c98092a09a55f56b9e6d699fd382f8c272c603
Thiago Cunha <thiagocborghi@gmail.com>
2021-04-10 10:41:25 -0300

    get artist and top tracks

--- src/index.js
+++ src/index.js
@@ -12,4 +12,21 @@ app.get("/search", async (req, res) => {
   return res.json(result);
 });
 
+app.get("/artist/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const result = await spotify.artist(id);
+
+  return res.json(result);
+});
+
+app.get("/artist/:id/top-tracks", async (req, res) => {
+  const id = req.params.id;
+  const params = req.query;
+
+  const result = await spotify.topTracks(id, params);
+
+  return res.json(result);
+});
+
 app.listen(process.env.PORT || "3000");
--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -14,13 +14,45 @@ class Spotify {
 
     const { data } = await api.get(`/search?${params}`);
 
-    const response = data.artists.items.map((item) => {
-      return {
-        name: item.name,
-      };
-    });
+    const response = data;
 
-    cache.set(params, response, 60 * 5);
+    cache.set(params, response, 60 * 1);
+
+    return response;
+  }
+
+  async artist(id) {
+    const cached = await cache.get(id);
+
+    if (cached) {
+      return cached;
+    }
+
+    const { data } = await api.get(`/artists/${id}`);
+
+    const response = data;
+
+    cache.set(id, response, 60 * 1);
+
+    return response;
+  }
+
+  async topTracks(id, options) {
+    const params = querystring.stringify(options);
+
+    const requestURL = `/artists/${id}/top-tracks?${params}`;
+
+    const cached = await cache.get(requestURL);
+
+    if (cached) {
+      return cached;
+    }
+
+    const { data } = await api.get(`/artists/${id}/top-tracks?${params}`);
+
+    const response = data;
+
+    cache.set(requestURL, response, 60 * 1);
 
     return response;
   }
